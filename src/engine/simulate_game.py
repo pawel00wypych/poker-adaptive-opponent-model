143644fe9f649c8fa31fc192eb08b0ab83d4482f
Pawel Wypych <pawelwypych4@gmail.com>
2026-03-20 19:39:24 +0100

    game simulation updated

--- src/engine/simulate_game.py
+++ src/engine/simulate_game.py
@@ -1,8 +1,10 @@
 from pypokerengine.api.game import setup_config, start_poker
 from src.agents.fish_player import FishPlayer
+from src.agents.aggressive_player import AggressivePlayer
 
-config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
-config.register_player(name="p1", algorithm=FishPlayer())
-config.register_player(name="p2", algorithm=FishPlayer())
-config.register_player(name="p3", algorithm=FishPlayer())
+config = setup_config(max_round=100, initial_stack=100, small_blind_amount=5)
+config.register_player(name="p1_fish", algorithm=FishPlayer("p1_fish"))
+config.register_player(name="p2_aggressive", algorithm=AggressivePlayer(
+    "p2_aggressive"))
 game_result = start_poker(config, verbose=1)
+print(f"\nGAME RESULT = {game_result}")
