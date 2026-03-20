5c0c09a747566f911ccd615183caddde97b915d6
Pawel Wypych <pawelwypych4@gmail.com>
2026-03-20 19:37:43 +0100

    simple fish player

--- src/agents/fish_player.py
+++ src/agents/fish_player.py
@@ -1,19 +1,37 @@
 from pypokerengine.players import BasePokerPlayer
+import random
 
 class FishPlayer(BasePokerPlayer):
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
 
     #  we define the logic to make an action through this method. (so this method would be the core of your AI)
     def declare_action(self, valid_actions, hole_card, round_state):
         # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
-        call_action_info = valid_actions[1]
-        action, amount = call_action_info["action"], call_action_info["amount"]
+        chance_to_call = random.random() * 100 + 1
+        if chance_to_call > 20:
+            call_action_info = valid_actions[1]  # 0 - fold 1 - call 2 - raise
+            action, amount = call_action_info["action"], call_action_info[
+                "amount"]
+        elif 5 < chance_to_call <= 20:
+            fold_action_info = valid_actions[0]  # 0 - fold 1 - call 2 - raise
+            action, amount = fold_action_info["action"], fold_action_info[
+                "amount"]
+        else:
+            raise_action_info = valid_actions[2]  # 0 - fold 1 - call 2 - raise
+            action, amount = raise_action_info["action"], raise_action_info[
+                "amount"]["min"]
+
         return action, amount   # action returned here is sent to the poker engine
 
     def receive_game_start_message(self, game_info):
         pass
 
     def receive_round_start_message(self, round_count, hole_card, seats):
-        pass
+        print(f"hole cards of player {self.name} in round {round_count} = "
+              f"{hole_card}")
 
     def receive_street_start_message(self, street, round_state):
         pass
@@ -22,5 +40,6 @@ class FishPlayer(BasePokerPlayer):
         pass
 
     def receive_round_result_message(self, winners, hand_info, round_state):
-        pass
-
+        print(f"\nround result message = {round_state}")
+        print(f"\n hand_info = {hand_info}")
+        print(f"\n winners = {winners}")
