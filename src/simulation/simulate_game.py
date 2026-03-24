07e4f81744a21c1e5a5afd723d7775acf85669a9
Pawel Wypych <pawelwypych4@gmail.com>
2026-03-24 12:57:08 +0100

    first game simulation saved to json file

--- src/simulation/simulate_game.py
+++ src/simulation/simulate_game.py
@@ -1,12 +1,21 @@
 from pypokerengine.api.game import setup_config, start_poker
 from src.agents.fish_player import FishPlayer
 from src.agents.aggressive_player import AggressivePlayer
+import json
+from pathlib import Path
+
+# Create a Path object
+result_path = Path("simulation_results/result.json")
 
 config = setup_config(max_round=100, initial_stack=100, small_blind_amount=5)
-config.register_player(name="p1_fish", algorithm=FishPlayer("p1_fish"))
+config.register_player(name="p1_fish", algorithm=FishPlayer(
+    player_name = "p1_fish"))
 config.register_player(name="p2_aggressive", algorithm=AggressivePlayer(
-    "p2_aggressive"))
-game_result1 = start_poker(config, verbose=1)
-game_result2 = start_poker(config, verbose=1)
-print(f"\nGAME RESULT_1 = {game_result1}")
-print(f"\nGAME RESULT_2 = {game_result2}")
+    player_name = "p2_aggressive"))
+game_result = start_poker(config, verbose=1)
+
+result_path.parent.mkdir(parents=True, exist_ok=True)
+with result_path.open("w") as f:
+    json.dump(game_result,
+              f,
+              indent=4)
