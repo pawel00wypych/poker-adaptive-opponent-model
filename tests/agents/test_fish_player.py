439dacbab5181e960630436a753cd12f28d23923
Pawel Wypych <pawelwypych4@gmail.com>
2026-03-31 19:26:17 +0200

    unit tests added

---
+++ tests/agents/test_fish_player.py
@@ -0,0 +1,19 @@
+from src.agents.fish_player import FishPlayer
+from pypokerengine.engine.poker_constants import PokerConstants as Const
+
+
+def test_declare_action_strong_preflop(valid_actions_conf):
+    hole_cards = ["H9", "S9"]
+    round_state = {"street": Const.Street.PREFLOP}
+    valid_actions = valid_actions_conf
+    fish = FishPlayer()
+    action, _ = fish.declare_action(valid_actions, hole_cards, round_state)
+    assert action == "call"
+
+def test_declare_action_medium_preflop(valid_actions_conf):
+    hole_cards = ['H7', 'S7']
+    round_state = {"street": Const.Street.PREFLOP}
+    valid_actions = valid_actions_conf
+    fish = FishPlayer()
+    action, _ = fish.declare_action(valid_actions,hole_cards,round_state)
+    assert action == 'fold'
