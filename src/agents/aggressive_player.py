adf1b1b04290dc4ae1301c42dc3c2715f6d935c7
Pawel Wypych <pawelwypych4@gmail.com>
2026-03-31 12:29:03 +0200

    using pypoker constants

--- src/agents/aggressive_player.py
+++ src/agents/aggressive_player.py
@@ -1,4 +1,5 @@
 from src.agents.player_template import PlayerTemplate
+from pypokerengine.engine.poker_constants import PokerConstants as Const
 import random
 
 class AggressivePlayer(PlayerTemplate):
@@ -9,15 +10,15 @@ class AggressivePlayer(PlayerTemplate):
     def declare_action(self, valid_actions, hole_card, round_state):
         chance_to_raise = random.random() * 100 + 1
         if chance_to_raise > 20:
-            raise_action_info = valid_actions[2] # 0 - fold 1 - call 2 - raise
+            raise_action_info = valid_actions[Const.Action.RAISE]
             action, amount = raise_action_info["action"], raise_action_info[
                 "amount"]["min"]
         elif 5 < chance_to_raise <= 20:
-            call_action_info = valid_actions[1]  # 0 - fold 1 - call 2 - raise
+            call_action_info = valid_actions[Const.Action.CALL]
             action, amount = call_action_info["action"], call_action_info[
                 "amount"]
         else:
-            fold_action_info = valid_actions[0]  # 0 - fold 1 - call 2 - raise
+            fold_action_info = valid_actions[Const.Action.FOLD]
             action, amount = fold_action_info["action"], fold_action_info[
                 "amount"]
 
