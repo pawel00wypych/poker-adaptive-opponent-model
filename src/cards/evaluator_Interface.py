a1c3083ffd8a81a75b4352b6888992b6d93e3c08
Pawel Wypych <pawelwypych4@gmail.com>
2026-04-03 14:57:57 +0200

    hand evaluator interface + tests

---
+++ src/cards/evaluator_Interface.py
@@ -0,0 +1,26 @@
+from PyPokerEngine.pypokerengine.engine.card import Card
+from PyPokerEngine.pypokerengine.engine.hand_evaluator import HandEvaluator
+
+
+class EvaluatorInterface:
+    @classmethod
+    def evaluate(cls, hole, community) -> dict:
+        """
+        Returns at least:
+        - 'score': integer, comparable for winner determination
+        - 'hand': optional dict with 'strength', 'high', 'low', etc.
+        """
+        hole = [cls.decode_str_to_card(c) for c in hole]
+        hand = [cls.decode_str_to_card(c) for c in community]
+        # HandEvaluator.eval_hand() returns integer in this
+        # format:
+        # [ hand strength bits ][ hand rank high ][ hand rank low ][ hole high ][ hole low ]
+        score = HandEvaluator.eval_hand(hole, hand)
+        hand_info = HandEvaluator.gen_hand_rank_info(hole, hand)
+        hand_info['score'] = score
+        return hand_info
+
+    @classmethod
+    def decode_str_to_card(cls, card: str) -> Card:
+        return Card.from_str(card)
+
