880236ccb0b3e52cbd60346519f3f4761259821b
Pawel Wypych <pawelwypych4@gmail.com>
2026-04-22 00:30:51 +0200

    RL training simulation

---
+++ src/simulation/training_simulation.py
@@ -0,0 +1,12 @@
+from src.agents.simple_agent import SimpleAgent
+from src.simulation.play_game_with_rl_player import play_game_with_rl_player
+
+agent = SimpleAgent()
+
+for episode in range(21):
+    # 1 hand = 1 episode
+    play_game_with_rl_player(agent) # runs PyPokerEngine game
+
+    if episode % 10 == 0:
+        print(f"Episode {episode}, memory size: {len(agent.memory)}")
+        print(f"memory: {agent.memory}")
