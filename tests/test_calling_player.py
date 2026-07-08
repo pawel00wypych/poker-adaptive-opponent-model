9e0c857f72a0075c170dba2632f69146f63f99e4
Pawel Wypych <pawelwypych4@gmail.com>
2026-07-08 20:40:46 +0200

    modified calling pl + tests

--- tests/test_calling_player.py
+++ tests/test_calling_player.py
@@ -72,3 +72,27 @@ def test_calling_player_uses_first_action_as_last_fallback():
 
     assert action == "raise"
     assert amount == {"min": 20, "max": 100}
+
+def test_calling_player_tracks_round_results():
+    player = CallingPlayer(player_name="calling")
+    player.uuid = "uuid-calling"
+
+    round_state_1 = {
+        "seats": [
+            {"name": "calling", "uuid": "uuid-calling", "stack": 100},
+            {"name": "opponent", "uuid": "uuid-opponent", "stack": 100},
+        ]
+    }
+
+    round_state_2 = {
+        "seats": [
+            {"name": "calling", "uuid": "uuid-calling", "stack": 120},
+            {"name": "opponent", "uuid": "uuid-opponent", "stack": 80},
+        ]
+    }
+
+    player.receive_round_result_message([], [], round_state_1)
+    player.receive_round_result_message([], [], round_state_2)
+
+    assert player.hands_played == 2
+    assert player.total_reward_bb == 2.0
