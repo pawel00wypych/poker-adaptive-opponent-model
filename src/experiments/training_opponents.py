4b177ef3a98ac364d4f518deaf5caad1abcecacf
Pawel Wypych <pawelwypych4@gmail.com>
2026-07-12 16:56:20 +0200

    new build opponent fun

--- src/experiments/training_opponents.py
+++ src/experiments/training_opponents.py
@@ -3,19 +3,46 @@ from src.players.calling_player import CallingPlayer
 from src.players.fish_player import FishPlayer
 
 
-def build_training_opponent(episode: int):
-    opponent_type = episode % 3
+SUPPORTED_OPPONENTS = {
+    "fish",
+    "aggressive",
+    "calling",
+}
 
-    if opponent_type == 0:
-        return "fish", FishPlayer(player_name="fish")
 
-    if opponent_type == 1:
-        return (
-            "aggressive",
-            AggressivePlayer(player_name="aggressive"),
+def build_opponent(opponent_type: str):
+    if opponent_type == "fish":
+        return FishPlayer(
+            player_name="fish",
         )
 
-    return (
+    if opponent_type == "aggressive":
+        return AggressivePlayer(
+            player_name="aggressive",
+        )
+
+    if opponent_type == "calling":
+        return CallingPlayer(
+            player_name="calling",
+        )
+
+    raise ValueError(
+        f"Unsupported opponent type: {opponent_type}"
+    )
+
+
+def build_training_opponent(episode: int):
+    opponent_types = [
+        "fish",
+        "aggressive",
         "calling",
-        CallingPlayer(player_name="calling"),
+    ]
+
+    opponent_type = opponent_types[
+        episode % len(opponent_types)
+    ]
+
+    return (
+        opponent_type,
+        build_opponent(opponent_type),
     )
